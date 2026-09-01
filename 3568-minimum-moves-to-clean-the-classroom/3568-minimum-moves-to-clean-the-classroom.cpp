1be3001ab43a81adb7c8a8e6360ff1ea4db593e6
Srushti Nerkar <58792071+srushtiinvent@users.noreply.github.com>
2026-09-01 11:50:07 +0530

    Time: 180 ms (93.88%), Space: 119.2 MB (87.76%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -1,84 +1,105 @@
-#include <vector>
-#include <string>
-#include <queue>
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int minMoves(vector<string>& classroom, int energy) {
         int m = classroom.size();
         int n = classroom[0].size();
-        
-        int start_r = -1, start_c = -1;
-        int K = 0; 
-        
-        vector<vector<int>> litter_id(m, vector<int>(n, -1));
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (classroom[i][j] == 'S') {
-                    start_r = i;
-                    start_c = j;
-                } else if (classroom[i][j] == 'L') {
-                    litter_id[i][j] = K++;
+        int sr = 0, sc = 0, K = 0;
+
+        vector<vector<int>> id(m, vector<int>(n, -1));
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (classroom[r][c] == 'S') {
+                    sr = r;
+                    sc = c;
+                }
+                else if (classroom[r][c] == 'L') {
+                    id[r][c] = K++;
                 }
             }
         }
 
         if (K == 0) return 0;
-        
-        int target_mask = (1 << K) - 1;
 
-        vector<int> max_energy(m * n * (1 << K), -1);
-        auto get_idx = [&](int r, int c, int mask) {
-            return (r * n + c) * (1 << K) + mask;
+        int states = 1 << K;
+        int target = states - 1;
+
+        // best[r][c][mask] = maximum energy seen for this state
+        vector<int> best(m * n * states, -1);
+
+        auto index = [&](int r, int c, int mask) {
+            return ((r * n + c) * states) + mask;
         };
 
-        queue<tuple<int, int, int, int>> q; 
-        q.push({start_r, start_c, 0, energy});
-        max_energy[get_idx(start_r, start_c, 0)] = energy;
+        struct State {
+            int r, c, mask, energy;
+        };
 
-        int steps = 0;
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
+        queue<State> q;
+
+        q.push({sr, sc, 0, energy});
+        best[index(sr, sc, 0)] = energy;
+
+        const int dr[] = {-1, 1, 0, 0};
+        const int dc[] = {0, 0, -1, 1};
+
+        int moves = 0;
 
         while (!q.empty()) {
             int sz = q.size();
-            
+
             while (sz--) {
                 auto [r, c, mask, e] = q.front();
                 q.pop();
 
-                if (max_energy[get_idx(r, c, mask)] > e) continue;
+                int curIdx = index(r, c, mask);
 
-                for (int d = 0; d < 4; ++d) {
+                if (best[curIdx] > e)
+                    continue;
+
+                for (int d = 0; d < 4; d++) {
                     int nr = r + dr[d];
                     int nc = c + dc[d];
 
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && classroom[nr][nc] != 'X') {
-                        int ne = e - 1; 
-                        if (ne < 0) continue; 
-                        int nmask = mask;
-                        
-                        if (classroom[nr][nc] == 'R') {
-                            ne = energy; 
-                        } else if (classroom[nr][nc] == 'L') {
-                            nmask |= (1 << litter_id[nr][nc]);
-                        }
+                    if (nr < 0 || nr >= m ||
+                        nc < 0 || nc >= n ||
+                        classroom[nr][nc] == 'X')
+                        continue;
 
-                        if (nmask == target_mask) {
-                            return steps + 1;
-                        }
+                    int ne = e - 1;
 
-                        int idx = get_idx(nr, nc, nmask);
-                        if (ne > max_energy[idx]) {
-                            max_energy[idx] = ne;
-                            q.push({nr, nc, nmask, ne});
-                        }
+                    if (ne < 0)
+                        continue;
+
+                    int nmask = mask;
+
+                    if (classroom[nr][nc] == 'L') {
+                        nmask |= (1 << id[nr][nc]);
+                    }
+
+                    if (classroom[nr][nc] == 'R') {
+                        ne = energy;
+                    }
+
+                    if (nmask == target)
+                        return moves + 1;
+
+                    int idx = index(nr, nc, nmask);
+
+                    if (ne > best[idx]) {
+                        best[idx] = ne;
+                        q.push({nr, nc, nmask, ne});
                     }
                 }
             }
-            steps++;
+
+            moves++;
         }
-        
-        return -1; 
+
+        return -1;
     }
 };
