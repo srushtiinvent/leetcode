18397f49525d24486019bbe91e69a072a25b3e68
Srushti Nerkar <58792071+srushtiinvent@users.noreply.github.com>
2026-09-02 22:58:27 +0530

    Time: 467 ms (93.98%), Space: 58.9 MB (59.21%) - LeetHub

--- 4041-minimum-operations-to-form-subset-sum-ii/4041-minimum-operations-to-form-subset-sum-ii.cpp
+++ 4041-minimum-operations-to-form-subset-sum-ii/4041-minimum-operations-to-form-subset-sum-ii.cpp
@@ -4,20 +4,28 @@ public:
         vector<int> dp(sum + 1, 1e9);
         dp[0] = 0;
         
+        int current_max_sum = 0; 
+        vector<int> min_cost(sum + 1, 1e9);
+        vector<int> valid_vals; 
+        valid_vals.reserve(150); 
         for (int x : nums) {
-            unordered_map<int, int> min_cost;
+            valid_vals.clear();
+            
             int d = 0;
             int curr = x;
+            int max_val_from_x = 0;
             
             while (true) {
                 int m = 0;
                 long long val = curr;
                 
                 while (val <= sum) {
-                    if (min_cost.find(val) == min_cost.end()) {
+                    if (min_cost[val] > d + m) {
+                        if (min_cost[val] == 1e9 && val > 0) {
+                            valid_vals.push_back(val);
+                        }
                         min_cost[val] = d + m;
-                    } else {
-                        min_cost[val] = min(min_cost[val], d + m);
+                        if (val > max_val_from_x) max_val_from_x = val;
                     }
                     
                     if (val == 0) break; 
@@ -30,21 +38,25 @@ public:
                 d++;
             }
             
-            vector<int> next_dp = dp;
-            for (auto& pair : min_cost) {
-                int v = pair.first;
-                int c = pair.second;
+            vector<int> next_dp = dp; 
+            
+            for (int v : valid_vals) {
+                int c = min_cost[v];
                 
-                if (v == 0) continue; 
-                
-                for (int j = 0; j <= sum - v; ++j) {
-                    if (dp[j] != 1e9) {
-                        next_dp[j + v] = min(next_dp[j + v], dp[j] + c);
+                for (int j = 0; j <= current_max_sum; ++j) {
+                    if (dp[j] != 1e9 && j + v <= sum) {
+                        if (dp[j] + c < next_dp[j + v]) {
+                            next_dp[j + v] = dp[j] + c;
+                        }
                     }
                 }
+                
+                min_cost[v] = 1e9;
             }
             
-            dp = next_dp;
+            dp = std::move(next_dp);
+            
+            current_max_sum = min(sum, current_max_sum + max_val_from_x);
         }
         
         return dp[sum] == 1e9 ? -1 : dp[sum];
